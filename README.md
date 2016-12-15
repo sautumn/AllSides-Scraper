66245e4641464bd985c9d2af3f4785d8c268bd01
Autumn Smith <autumn07@gmail.com>
2016-12-14 20:54:43 -0800

    added finished scraper to github

---
+++ README.md
@@ -0,0 +1,19 @@
+## AllSides Scraper
+
+Script that uses Cheerio and Request libraries to scrape AllSide.com to get bias ratings for new sites.
+
+## Motivation
+
+Used in conjunction with NewsGate, a project that I worked on as part of a group at HackReactor to detect fake news via a chrome extension.
+
+## Installation
+
+Run npm install and then npm start to run the script. The results will be written into the biasRatings file.
+
+## Future Plans
+
+Replace the callbacks with promises and clean up the code further. Add other data values such as the wikipedia article and the summary.
+
+Thanks for reading! :)
+
+-Autumn Smith
---
+++ newsscrape.js
@@ -0,0 +1,78 @@
+var request = require('request');
+// var rp = require('request-promise'); //maybe later :)
+var cheerio = require('cheerio');
+var lodash = require('lodash');
+var underscoreDeepExtend = require('underscore-deep-extend');
+var fs = require('fs');
+
+var file = fs.createWriteStream('biasRatings.txt');
+
+var _ = require('lodash');
+_.mixin({deepExtend: underscoreDeepExtend(_)});
+
+var data = {};
+
+var options = { method: 'GET',
+  url: 'http://www.allsides.com/bias/bias-ratings',
+  qs: 
+   { field_news_source_type_tid: '2',
+     field_news_bias_nid: '1',
+     field_featured_bias_rating_value: 'All',
+     title: ''
+   },
+  form: {} };
+
+['', '1', '2'].forEach(function(page) {
+  
+  var optionsPages = _.deepExtend(options, {qs: {page: page}});
+
+  request(optionsPages, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    var $ = cheerio.load(body);
+
+    var dataObj = {title: null, rating: null, url: null};
+
+    $('.source-title').each(function(i, element) {
+      var title = element.parent.children[0].next.children[0].next.children[0].data;
+      var rating = element.parent.children[2].next.children[0].next.children[0].attribs.title;
+
+      var rating = rating.split(': ');
+
+
+      var link = element.parent.children[0].next.children[0].next.children[0].parent.attribs.href;
+      
+      //we must go deeper to get the urls!!
+
+      var pageLink = 'http://www.allsides.com/' + link;
+
+      var options = { method: 'GET',
+        url: pageLink
+      };
+
+      request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+          var $ = cheerio.load(body);
+
+          $('.www').each(function (i, element) {
+
+            var url = element.attribs.href;
+
+            var domain = url.replace(/^https?:\/\//,''); // Strip off https:// and/or http://
+            domain = domain.replace(/^www\./,''); // Strip off www.
+            domain = domain.split('/')[0]; // Get the domain and just the domain (not the path)
+
+            data[domain] = {title: title, rating: rating[1], url: url}; 
+          });
+
+          //write to file
+          fs.writeFile('biasRatings.json', JSON.stringify(data), function (err) {
+            if (err) throw new Error(error);
+            console.log('Writing to file...');
+          });
+      });
+    });
+  });
+})
+
